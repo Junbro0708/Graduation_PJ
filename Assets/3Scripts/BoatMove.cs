8e307d9719f85a2b6ec6da7078df1ae5917b24ea
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-13 15:28:21 +0900

    Share

---
+++ Assets/3Scripts/BoatMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMove : MonoBehaviour
+{
+    public float speed;
+    public float time = 0; 
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time < 3)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }else if(time > 3 && time < 6)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (time > 8)
+            time = 0;
+
+        Debug.Log(time);
+    }
+}
