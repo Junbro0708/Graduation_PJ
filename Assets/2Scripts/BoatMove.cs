19fd53dbe53ed4ffd055b7b43ce3a112f43cb6cf
Junbro0708 <qkrwns708@soongsil.ac.kr>
2021-01-18 20:22:18 +0900

    Anim PK Manager

--- Assets/2Scripts/BoatMove.cs
+++ Assets/2Scripts/BoatMove.cs
@@ -11,16 +11,22 @@ public class BoatMove : MonoBehaviour
     {
         time += Time.deltaTime;
 
-        if (time < 4)
+        if (time < 6)
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }else if(time > 4 && time < 7)
+        }else if(time > 6 && time < 9)
+        {
+            
+        }else if(9 < time && time < 21)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }else if(time > 21)
+        {
+
         }
 
-        if (time > 9)
-            time = 0;
+        if (time > 21)
+            time = -6;
 
         Debug.Log(time);
     }
